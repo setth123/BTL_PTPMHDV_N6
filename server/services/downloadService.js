db5f48f8b1e106833a60c3b106fbb366d26aa7b4
TrangDiep04 <diepkieutrang2004@gmail.com>
2024-12-07 20:48:48 +0700

    Update downloadService.js

--- server/services/downloadService.js
+++ server/services/downloadService.js
@@ -1,23 +1,14 @@
-import fs from 'fs';
-import path from 'path';
 import xlsx from 'xlsx';
 
-function createExcelFile(data, fileName) {
+function createExcelFile(data) {
     const workbook = xlsx.utils.book_new();
     const worksheet = xlsx.utils.json_to_sheet(data);
     xlsx.utils.book_append_sheet(workbook, worksheet, 'Schedule');
 
-    // Đảm bảo thư mục uploads tồn tại
-    const uploadsDir = path.join(process.cwd(), 'uploads');
-    if (!fs.existsSync(uploadsDir)) {
-        fs.mkdirSync(uploadsDir);
-    }
-
-    // Lưu file tại thư mục uploads
-    const filePath = path.join(uploadsDir, fileName);
-    xlsx.writeFile(workbook, filePath);
-
-    return filePath; // Trả về đường dẫn file đã tạo
+    // Tạo buffer từ workbook
+    const buffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    
+    return buffer; // Trả về buffer thay vì lưu vào file
 }
 
 export default { createExcelFile };
